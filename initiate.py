6ca72c9e564f17cd4ee9e14f9fc08c1a210f7e74
Anton Fougt <afougt@gmail.com>
2014-03-21 11:14:19 +0100

    fixed a return function of the set Variables

--- initiate.py
+++ initiate.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class set_values(object):
-	
-	def __init__(self):
-		self.var = {'width':800,'height':600}
-	
-	def config(self):
-		pass
 
-	def args(self):
-		pass
+class setValues(object):
+    
+    def __init__(self):
+        self.var = {'width':800,'height':600}
 	
+    def _config(self):
+        pass
+
+    def _args(self):
+        pass
+	
+    def setVars(self):
+        return self.var
+
+defVars = setValues().setVars()
+
 def main():
-	
-	return 0
-
+    
+    return 0
+        
 if __name__ == '__main__':
-	main()
+    main()
 
