1954a1dc186b9fb9b4fe9bcb8cb713d4718f8a31
AF <sagan_om_ringen@hotmail.com>
2014-03-26 19:59:58 +0100

    Fixed indentation

--- initiate.py
+++ initiate.py
@@ -7,13 +7,13 @@ class setValues(object):
     def __init__(self):
         self.var = {'width'   :800,   'height' :600,
                     'treeSide':'left','toolBar':'top'}
-	
+
     def _config(self):
         pass
 
     def _args(self):
         pass
-	
+
     def setVars(self):
         return self.var
 
