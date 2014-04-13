87a9023354077d198a365c2babb65d88fd4a89c2
Anton Fougt <sagan_om_ringen@hotmail.com>
2014-04-13 16:14:02 +0200

    small fixes in indentation

--- dbHandlers/database_handler_sqlite.py
+++ dbHandlers/database_handler_sqlite.py
@@ -4,30 +4,30 @@
 import sqlite3 as sqlite
 
 name = 'Sqlite 3'
-options = {'login':False}
+
+options = {'login':False,
+			'foreign':True}
 
 class database_handler_sqlite(object):
-	
+
     def __init__(self, path):
-		
- #       defValues = {'foreignkeys':0}                       #Set the intital values ex. Pragma foreign
-        
         self.path = path
         self.db_type = 'sqlite'
         
     def connect(self):
         self.con = sqlite.connect(self.path)
         self.cur = self.con.cursor()
-		
+
     def exec_sql(self):
         pass
         
-    def update_tables():
+    def update_tables(self):
         pass
-	
+
+
 def main():
-	pass
+    pass
 
 if __name__ == '__main__':
-	main()
+    main()
 
