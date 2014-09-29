c15eec936fd0bb2c20ad6fd284f61efbb43d32e1
Anton Fougt <sagan_om_ringen@hotmail.com>
2014-09-29 06:58:06 +0200

    Updated with roadmap

--- Main.py
+++ Main.py
@@ -5,6 +5,16 @@ import tkinter
 import gui.Gui
 
 
+# Check OS
+
+# Set up OS specifics, templates etc.
+
+# Get current working directory, config path etc.
+
+# Init args and config
+
+
+
 def main():
     root = tkinter.Tk()
     gui.Gui.mainGuiFrame(master=root).mainloop()
