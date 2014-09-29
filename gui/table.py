a5e22db12e5b18046b0adf0cde6a3c2acf1dd417
Anton Fougt <sagan_om_ringen@hotmail.com>
2014-09-29 06:57:36 +0200

    Table layout

---
+++ gui/table.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import ttk
+
+class TableCanvas(tk.Canvas):
+    
+    def __init__(self):
+        pass
+    
+    def initialiseFields(self):
+        pass
+    
+    def adjustColumnWidths(self):
+        pass
+    
+    def autoResizeColumns(self):
+        pass
+    
+    def set_colPositions(self):
+        pass
+    
+    def addRow(self):
+        pass
+
+
+class TableModel(object):
+    
+    def __init__(self):
+        pass
+    
+    def setupModel(self):
+        pass
+    
+    def initialiseFields(self):
+        pass
+    
+    def addRow(self):
+        pass
